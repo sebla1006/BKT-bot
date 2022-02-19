4ff653ed24797b4930351617107d455626af86ac
Sebla <sebla1006@gmail.com>
2022-02-19 13:40:46 +0100

    Add files via upload

---
+++ embedCreate.js
@@ -0,0 +1,29 @@
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
+
+/**
+ * Fontion qui envoie l'embed ticket dans le channel
+ * @param { guildTextChannel } channel Le salon du où l'embed sera envoyé
+ */
+
+const creator = ( channel ) => {
+    const embed = new MessageEmbed()
+        .setTitle("Recrutement")
+        .setDescription("Pour créer un ticket réagissez avec ✉️.")
+        .setFooter({text: "BKT esport" , iconURL: "https://i.imgur.com/Xu89RO6.jpg"})
+        .setColor("#8F51C5")
+
+    const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setStyle("SECONDARY")
+                .setLabel("Créer un ticket")
+                .setCustomId("ticket")
+                .setEmoji("✉️")
+        )
+            
+    channel.send( {embeds: [embed], components: [row]} )   
+}
+
+module.exports = {
+    creator
+}
---
+++ index.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton, Permissions } = require("discord.js");
+const client = new Client({ intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ]});
+const { guildId, messageId, channelId, categoryChannelId } = require('./config.json');
+const { creator } = require("./embedCreate");
+const token = process.env.TOKEN;
+
+client.login(token);
+client.ticket = []
+
+client.on('ready', async (client) => {
+    console.log(`Le bot est connecté en tant que ${client.user.tag}.`);
+    const guild = await client.guilds.fetch(guildId);
+    const channel = await guild.channels.fetch(channelId);
+    //creator(channel);
+});
+
+client.on('interactionCreate', async (interaction) => {
+    if(!interaction.isButton()) return;
+    if(interaction.message.id === messageId){
+        if(!client.ticket.includes(interaction.member.id)){
+            await interaction.reply( {ephemeral: true, content: "Le ticket est en cours de création."})
+            client.emit("ticketCreate", (interaction.member))
+        }else {
+            await interaction.reply( {ephemeral: true, content: ":x: Vous ne pouvez pas créer plusieurs ticket."})
+        }
+    }
+    if(interaction.channel.parentId === categoryChannelId && interaction.customId === "close"){
+        if(client.ticket.includes(interaction.member.id)) return await interaction.reply({ephemeral: true, content: ":x: Vous n'avez pas la permission de fermer le ticket."})
+        await interaction.reply(`Le salon est en cours de suppression.`)
+        client.ticket.splice(client.ticket.indexOf(interaction.message.mentions.members.first().id), 1)
+        setTimeout( function(){
+            interaction.channel.delete()
+        }, 2000)
+        
+    }
+});
+
+client.on('ticketCreate', async (member) => {
+    client.ticket.push(member.id)
+    const parent = await member.guild.channels.fetch(categoryChannelId);
+    member.guild.channels.create(member.nickname || member.user.username, { 
+        parent: parent,
+        permissionOverwrites: [
+            {
+                id: member.id,
+                allow: [ Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES ]
+            }
+        ]
+    })
+    .then( (channel) => {
+        const embed = new MessageEmbed() 
+            .setDescription("Le staff sera à vous dans quelques instants. Envoyez votre candidature ici.\nPour fermer le ticket cliquer sur 🔒.")
+            .setFooter({text: "BKT esport" , iconURL: "https://i.imgur.com/Xu89RO6.jpg"})
+            .setColor("#8F51C5")
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setStyle("SECONDARY")
+                    .setLabel("Fermer le ticket")
+                    .setCustomId("close")
+                    .setEmoji("🔒")
+            )
+        
+            channel.send({ embeds: [embed], components: [row], content: `Bienvenue ${member}`})
+    })
+});
